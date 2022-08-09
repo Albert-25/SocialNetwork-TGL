560f17d2e472f9122aebf520232ec56342bdc506
Albert-25 <ajst_95@outlook.com>
2022-08-09 11:31:28 -0500

    configuracion vite agregada

--- api/app.js
+++ api/app.js
@@ -4,9 +4,12 @@ require('dotenv').config()
 const router = require("./src/routes/index");
 const app = express()
 
-
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.get('*', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+})
 
 module.exports = app;
