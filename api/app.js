9c72886f624ca2b70ea5ca76830fa30e90b05d17
Albert-25 <ajst_95@outlook.com>
2022-08-17 17:41:01 -0500

    cors agregados al server

--- api/app.js
+++ api/app.js
@@ -7,39 +7,40 @@ const router = require("./src/routes/index");
 const app = express()
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.use(express.json());
 app.use(router);
 
 const serverHttp = http.createServer(app)
 const io = new Server(serverHttp, {
     cors: {
-        origin: "http://localhost:5173"// url del frontend 
+        origin: "*"// url del frontend 
     }
 })
+
+let users = [];
 
 const getUser = (userId) => {
     return users.find(user => user.userId == userId)
 }
-
-let users = [];
-
 const addUser = (userId, socketId) => {
     !users.some(user => user.userId == userId) &&
         users.push({ userId, socketId })
 }
-
-
 const removeUser = (socketId) => {
     users = users.filter(user => user.socketId != socketId)
 }
-
 io.on("connection", (socket) => {
-
     socket.on("addUser", userId => {
         addUser(userId, socket.id)
         io.emit("getUsers", users)
     })
-
     socket.on("sendMessage", ({ senderId, receiverId, text }) => {
         const user = getUser(receiverId)
         user?.socketId && io.to(user.socketId).emit("getMessage", {
@@ -47,14 +48,11 @@ io.on("connection", (socket) => {
             text
         })
     })
-
     socket.on("disconnect", () => {
         console.log("a user is disconnected ")
         removeUser(socket.id)
         io.emit("getUsers", users)
     })
 })
-
-
 
 module.exports = serverHttp;
