a8cc9c46af698e4108d3b2f12f94b0f379f67c6d
Albert-25 <ajst_95@outlook.com>
2022-08-17 21:22:34 -0500

    api ultima retornada

--- api/src/db/sequelize.js
+++ api/src/db/sequelize.js
@@ -4,6 +4,8 @@ const setupModels = require("./init.db");
 const { User } = require("./models/userSchema");
 const { Post } = require("./models/postSchema");
 const { Comment } = require("./models/commentSchema");
+const { Message } = require("./models/messageSchema");
+const { Conversation } = require("./models/conversationSchema");
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = require("../config/index");
 
@@ -13,20 +15,20 @@ const sequelize = new Sequelize(
     URI,
     {
         dialect: 'postgres',
-        pool: {
-            max: 3,
-            min: 1,
-            idle: 10000,
-        },
-        dialectOptions: {
-            ssl: {
-                require: true,
-                ///
-                rejectUnauthorized: false,
-            },
-            keepAlive: true,
-        },
-        ssl: true
+        // pool: {
+        //     max: 3,
+        //     min: 1,
+        //     idle: 10000,
+        // },
+        // dialectOptions: {
+        //     ssl: {
+        //         require: true,
+        //         ///
+        //         rejectUnauthorized: false,
+        //     },
+        //     keepAlive: true,
+        // },
+        // ssl: true
     })
 
 setupModels(sequelize);
@@ -38,9 +40,14 @@ User.hasMany(Comment);
 Comment.belongsTo(User);
 Post.hasMany(Comment);
 Comment.belongsTo(Post);
+User.hasMany(Message);
+Message.belongsTo(User);
+Conversation.hasMany(Message);
+Message.belongsTo(Conversation);
 
 sequelize.sync({
-    force: false
+    force: false,
+    alter: true
 })
 
 module.exports = sequelize;
