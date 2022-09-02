83e6dbe583eea291c91d492584c4fb3b9b4cd1b5
Albert-25 <ajst_95@outlook.com>
2022-09-02 16:51:27 -0500

    documentation of user routes added

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path")
 
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerJsDocs = YAML.load(path.join(__dirname, "./api.yaml"))
 
 const userRouter = require("./userRouter");
 const postsRouter = require("./postsRouter")
@@ -10,6 +14,43 @@ const favoritesRouter = require("./favoritesRouter");
 const conversationsRouter = require("./conversationsRouter")
 const messagesRouter = require("./messagesRouter")
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: object
+ *          properties:
+ *              alias:
+ *                  type:string
+ *                  description: user alias
+ *              name:
+ *                  type:string
+ *                  description: user name
+ *              email:
+ *                  type:string
+ *                  description: user email
+ *              phone:
+ *                  type:string
+ *                  description: user phone
+ *              password:
+ *                  type:string
+ *                  description: user password
+ *              required:
+ *              - alias
+ *              - name
+ *              - email
+ *              - phone
+ *              - password       
+ *              example:
+ *                  alias: Albert-25
+ *                  name: Albert Smith
+ *                  email: albert.smith@email.com
+ *                  phone: 0123456789
+ *                  password: password
+ */
+
+
 router.use("/user", userRouter)
 router.use("/posts", postsRouter)
 router.use("/comments", commentsRouter)
@@ -17,5 +58,6 @@ router.use("/friends", friendsRouter)
 router.use("/favorites", favoritesRouter)
 router.use("/conversations", conversationsRouter)
 router.use("/messages", messagesRouter)
+router.use("/", swaggerUI.serve, swaggerUI.setup(swaggerJsDocs))
 
 module.exports = router
