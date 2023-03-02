4cfc56f18bb0a48e0309a7aaa5a49ada82362f22
Albert-25 <ajst_95@outlook.com>
2023-03-02 13:27:03 -0500

    vercel refresh

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 serverHttp = require("./app");
 const { PORT } = require("./src/config/index");
 
+// haciendo un commit para que los cambios en vercel surtan efecto
+
 serverHttp.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 })
